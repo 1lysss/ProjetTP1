eba46974348a49d081aebf4cb17012c017707fd2
Ilyass <irrifiilyass@gmail.com>
2025-04-20 15:35:56 +0100

    Add files via upload

---
+++ TP1/script.js
@@ -0,0 +1,16 @@
+document.getElementById('form-exam').addEventListener('submit', function(e){
+    e.preventDefault();
+    const examen ={
+        nom: document.getElementById('exam-nom').value,
+        duree: parseInt(document.getElementById('duree').value),
+        description: document.getElementById('description').value,
+        proprietaire: document.getElementById('proprietaire').value,
+        questions: []
+    };
+    const examsKey = 'examens_' + examen.proprietaire;
+    const exams = JSON.parse(localStorage.getItem(examsKey)) || [];
+    exams.push(examen);
+    localStorage.setItem(examsKey, JSON.stringify(exams));
+    alert('Examen ajouté !');
+    this.reset();
+});
